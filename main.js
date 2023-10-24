53c76f80fd14c67eb94022679c64df386436f7f3
sanavalos <santiago.avalos@outlook.com>
2023-10-23 21:11:28 -0300

    feat: remove unnecessary dollar rates

--- main.js
+++ main.js
@@ -1,40 +1,24 @@
 
 const { createApp } = Vue
 createApp({ 
-
     data(){
         return{
-        
             error: false,
             url: "https://dolarapi.com/v1/dolares",
             datos:[]
-          
-
         }
     }, 
     methods:{
         fetchData(url){
             fetch(url)
             .then(response => response.json())
-            .then(data => {console.log(data)
-            
-            
-            this.datos= data;
-           
-
+            .then(data => {
+            this.datos= data.filter(e => e.casa === "oficial" || e.casa === "blue" || e.casa === "mayorista");
             }
             )
-            
-           
         }
-
     },
-
     created() {
         this.fetchData(this.url)
     },
-
-
-
-
 }).mount('#app')
