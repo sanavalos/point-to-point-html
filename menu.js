d211dd7b32def834868597b1be47ee3c798f88b0
sanavalos <santiago.avalos@outlook.com>
2023-10-25 20:37:05 -0300

    refactor: files naming

--- menu.js
+++ menu.js
@@ -2,13 +2,10 @@ const button = document.querySelector('.buttonHamb')
 const pagesMovile = document.querySelector('.pages-movile')
 
 button.addEventListener('click',()=>{
-    console.log('controlando')
     if (pagesMovile.classList.contains("active")){
         pagesMovile.classList.remove('active')
     }
     else{
         pagesMovile.classList.add('active')
     }
-    
-    
 })
